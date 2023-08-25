a866dc1763d86c4ac6bbf9fb5983059aba96b1fa
SujatAhmed <sujatahmed842002@gmail.com>
2023-08-25 19:11:47 +0600

    seperated the classes

--- problem9.1/Stock.java
+++ problem9.1/Stock.java
@@ -21,22 +21,3 @@ public class Stock {
 		
 	}
 }
-class stock{
-	String symbol;
-	String name;
-	double current_price;
-	double previous_price;
-	stock(String s, String n, double c, double p){
-		symbol = s;
-		name = n;
-		current_price = c;
-		previous_price = p;
-
-
-	}
-   double get_percentagechange() {
-
-		return ((current_price - previous_price)/current_price)*100;
-	}
-
-}
