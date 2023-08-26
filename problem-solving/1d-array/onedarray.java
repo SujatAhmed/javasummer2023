f617a41e8818357ccf47d2183848f6cd5d7d01bf
SujatAhmed <sujatahmed842002@gmail.com>
2023-08-26 18:24:47 +0600

    finally made decent progress

--- problem-solving/1d-array/onedarray.java
+++ problem-solving/1d-array/onedarray.java
@@ -1,8 +1,7 @@
 public class onedarray {
 	public static void main(String[] args) {
 		
-		bool check;
-		int[] arr = {1,0,1,0,0,1,0,0,1,1};
+		int[] arr = {1,0,1,0,0,1,1,1,1,1};
 		int leap = 3;
 		int[] posarray = new int[10];
 		int[] specialarray= new int[10];
@@ -22,26 +21,39 @@ public class onedarray {
 
 		}
 
-		System.out.println(function(0, leap, posarraynull, specialarraynull));
+		System.out.println(function(0, leap, posarray, specialarray));
+
 	}
 
-	public static bool function(int position1, int leap, int[] posarraynull,int[] specialarraynull) {
+	public static int function(int position1, int leap, int[] posarraynull,int[] specialarraynull ) {
 		int newposition1 = position1;
-		int newpositon2 = position1; 
+		int newposition2 = position1; 
 		newposition1++;
-		newpositon2= newpositon2+leap;
+		newposition2= newposition2+leap;
+		int[] newarray = new int[10];
+		int[] newposarray = new int[10];
+
+
+		for (int i = 0; i < newarray.length; i++) {
+			newarray[i]=specialarraynull[i];
+			
+		}
+		for (int i = 0; i < newposarray.length; i++) {
+			newposarray[i]=posarraynull[i];
+			
+		}
 		
 		for(int i = 0; i<10; i++){
-			if(newposition1==specialarraynull[i] || newpositon2==specialarraynull[i]){
-				check = true;
-				return check;
-				break;
+			if(newposition1==newarray[i] || newposition2==newarray[i]){
+				return 1;
+ 
+
 			}
 		}
 
 		for (int i = 0; i <10 ; i++) {
 			if(newposition1==posarraynull[i]){
-				function(newposition1, leap, posarraynull, specialarraynull);
+				 return function(newposition1, leap, newposarray, newarray );
 				
 
 			}
@@ -50,14 +62,16 @@ public class onedarray {
 
 		for (int i = 0; i <10 ; i++) {
 			if(newposition2==posarraynull[i]){
-				function(newposition2, leap, posarraynull, specialarraynull);
+				 return function(newposition2, leap, newposarray, newarray );
 				
 
 			}
 			
 		}
 
-		
+		return 0;	
+
+
 		
 	}
 }
