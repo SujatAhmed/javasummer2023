5ee0198cafa33260acb87989eb872bc06efb640e
SujatAhmed <sujatahmed842002@gmail.com>
2023-08-29 22:23:03 +0600

    did homework 8

--- labs/lab8/student.java
+++ labs/lab8/student.java
@@ -2,16 +2,59 @@ import java.util.*;
 /**
  * hw801
  */
-public class hw801 {
+public class student{
 	public static void main(String[] args) {
+		student s1 = new student("sujat","2231177","cse");
+		System.out.println(s1.getname());
+		System.out.println(s1.getID());
+		System.out.println(s1.getDept());
+		s1.setName("Notsujat");
+		s1.setID("not2231177642");
+		s1.setdept("notCSE");
+		System.out.println(s1.getname());
+		System.out.println(s1.getID());
+		System.out.println(s1.getDept());
+
+		System.out.println("student info ");
+		s1.printinfo();
+		
+	
+
 		
 	}
-	String name;
-	String ID;
-	String dept;
-	hw801(){
+	private String name;
+	private String ID;
+	private String dept;
+	student(){
 
 	}
+	student(String name, String ID, String dept){
+		this.name = name;
+		this.ID = ID;
+		this.dept = dept;
+	}
+	void setName(String name){
+		this.name = name;
+	}
+	String getname(){
+		return name;
+	}
+	void setID(String ID){
+		this.ID = ID;
+	}
+	String getID(){
+		return ID;
+	}
+	void setdept(String dept){
+		this.dept = dept;
+	}
+	String getDept(){
+		return dept;
+	}
+	void printinfo(){
+		System.out.println(name);
+		System.out.println(dept);
+		System.out.println(ID);
 
-	
+	}
 }
